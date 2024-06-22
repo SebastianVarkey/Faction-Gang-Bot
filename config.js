f0ddaa711fb49df801f2f553ba13ea2b3521bfe7
Sebastian Varkey <anitkp3k@gmail.com>
2024-06-22 23:08:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: 'MTI1NDAyNTgxMDY5MjY2OTU5MA.GunKe5.hxZdic8vwA5LWPZQ34HjwBJjdQcmtz4DmPeoqM',
-    clientId: '1254025810692669590',
-    guildId: '1240546156577030164' // optional, for testing in a specific guild
+    token: 'MTI1NDAyNTgxMDY5MjY2OteDJDbfJTU5MA.GunKe5.hxZdic8vwA5LWPZQ34HjwBJjdQcmtz4DmPeoqM',
+    clientId: '125402581069342669590',
+    guildId: '124054615657704322430164' // optional, for testing in a specific guild
 };
