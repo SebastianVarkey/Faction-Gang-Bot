0ab1bec5845bcf3df7117d83f388196b05961973
Sebastian Varkey <anitkp3k@gmail.com>
2024-06-22 23:07:58 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+    token: 'MTI1NDAyNTgxMDY5MjY2OTU5MA.GunKe5.hxZdic8vwA5LWPZQ34HjwBJjdQcmtz4DmPeoqM',
+    clientId: '1254025810692669590',
+    guildId: '1240546156577030164' // optional, for testing in a specific guild
+};
